20f5ce81dfde49ec969252c5bfedb6ad535d9255
Carson R <carsonaurum1@icloud.com>
2023-02-09 08:30:43 -0600

    boost_test setup

---
+++ tests/TestMain.cxx
@@ -0,0 +1,14 @@
+//
+// Created by Carson Rau on 2/8/23.
+//
+
+#define BOOST_TEST_MODULE MainModule
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(test_main)
+    BOOST_AUTO_TEST_CASE(first_test)
+    {
+        BOOST_TEST(true);
+    }
+BOOST_AUTO_TEST_SUITE_END()
